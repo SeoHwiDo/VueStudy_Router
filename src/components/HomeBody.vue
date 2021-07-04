128fca2730d0ed9446ff8b3c7b82e91057e27105
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-07-04 15:23:49 +0900

    add home components[add]

--- src/components/HomeBody.vue
+++ src/components/HomeBody.vue
@@ -1,22 +1,24 @@
 <template>
     <v-app>
         <v-container fluid>
-            <v-flex xs4 class="text-xs-center my-2"><div class="poster"><img src="../assets/home_body_poster.jpg" alt=""></div></v-flex>
-            <v-flex xs4 class="text-xs-center my-2">
-                <div>
-                    <span class="display-1 font-weight-bold my-2">마음의 순례길 행사 및 공지</span>
+            <v-layout row wrap>
+                <v-flex md4><div class="poster"><img src="../assets/home_body_poster.jpg" alt=""></div></v-flex>
+                <v-flex md4>
                     <div>
-                        <span>2022년 3월 2일 정기 걷기</span>
+                        <span class="display-1 font-weight-bold my-2">마음의 순례길 행사 및 공지</span>
+                        <div>
+                            <span>2022년 3월 2일 정기 걷기</span>
+                        </div>
+                         <div>
+                            <span>마음의 순례길 공식 어플리케이션 출시</span>
+                        </div>
+                         <div>
+                            <span>마음의 순례길 사진 공모전 개최</span>
+                        </div>
                     </div>
-                     <div>
-                        <span>마음의 순례길 공식 어플리케이션 출시</span>
-                    </div>
-                     <div>
-                        <span>마음의 순례길 사진 공모전 개최</span>
-                    </div>
-                </div>
-            </v-flex>
-            <v-flex xs4 class="text-xs-center my-2"><div class="content"><img src="../assets/home_body_content.jpg" alt=""></div></v-flex>
+                </v-flex>
+                <v-flex md4><div class="content"><img src="../assets/home_body_content.jpg" alt=""></div></v-flex>
+        </v-layout>
         </v-container>
     </v-app>
 </template>
